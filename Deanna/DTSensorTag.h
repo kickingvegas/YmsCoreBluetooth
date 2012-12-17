1151fc263635814d426c2396438283fd110ff2a0
Charles Choi <charles.choi@yummymelon.com>
2012-12-17 10:59:38 -0800

    Initial Entry.

---
+++ Deanna/DTSensorTag.h
@@ -0,0 +1,13 @@
+//
+//  DTSensorTag.h
+//  Deanna
+//
+//  Created by Charles Choi on 12/17/12.
+//  Copyright (c) 2012 Yummy Melon Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DTSensorTag : NSObject
+
+@end
---
+++ Deanna/DTSensorTag.m
@@ -0,0 +1,13 @@
+//
+//  DTSensorTag.m
+//  Deanna
+//
+//  Created by Charles Choi on 12/17/12.
+//  Copyright (c) 2012 Yummy Melon Software. All rights reserved.
+//
+
+#import "DTSensorTag.h"
+
+@implementation DTSensorTag
+
+@end
