39ecb36010ba6729ffe6d0a018102c6ba34d155e
Charles Choi <charles.choi@yummymelon.com>
2012-12-17 10:59:52 -0800

    Initial Entry.

---
+++ Deanna/DTHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  DTHomeViewController.h
+//  Deanna
+//
+//  Created by Charles Choi on 12/17/12.
+//  Copyright (c) 2012 Yummy Melon Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DTHomeViewController : UIViewController
+
+@end
---
+++ Deanna/DTHomeViewController.m
@@ -0,0 +1,38 @@
+//
+//  DTHomeViewController.m
+//  Deanna
+//
+//  Created by Charles Choi on 12/17/12.
+//  Copyright (c) 2012 Yummy Melon Software. All rights reserved.
+//
+
+#import "DTHomeViewController.h"
+
+@interface DTHomeViewController ()
+
+@end
+
+@implementation DTHomeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
