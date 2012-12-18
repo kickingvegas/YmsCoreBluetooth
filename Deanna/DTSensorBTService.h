697675a93d3082b6d7bd3799854c6f64de856fe9
Charles Choi <charles.choi@yummymelon.com>
2012-12-18 13:10:32 -0800

    Initial Entry.

---
+++ Deanna/DTSensorBTService.h
@@ -0,0 +1,13 @@
+//
+//  DTSensorBTService.h
+//  Deanna
+//
+//  Created by Charles Choi on 12/18/12.
+//  Copyright (c) 2012 Yummy Melon Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DTSensorBTService : NSObject
+
+@end
---
+++ Deanna/DTSensorBTService.m
@@ -0,0 +1,13 @@
+//
+//  DTSensorBTService.m
+//  Deanna
+//
+//  Created by Charles Choi on 12/18/12.
+//  Copyright (c) 2012 Yummy Melon Software. All rights reserved.
+//
+
+#import "DTSensorBTService.h"
+
+@implementation DTSensorBTService
+
+@end
