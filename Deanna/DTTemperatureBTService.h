8c6381e290392a38ba3cfa20543f509dbbc9bc2e
Charles Choi <charles.choi@yummymelon.com>
2012-12-18 16:40:31 -0800

    Initial Entry.

---
+++ Deanna/DTTemperatureBTService.h
@@ -0,0 +1,16 @@
+//
+//  DTTemperatureBTService.h
+//  Deanna
+//
+//  Created by Charles Choi on 12/18/12.
+//  Copyright (c) 2012 Yummy Melon Software. All rights reserved.
+//
+
+#import "DTSensorBTService.h"
+
+@interface DTTemperatureBTService : DTSensorBTService
+
+- (id)initWithName:(NSString *)oName;
+
+
+@end
---
+++ Deanna/DTTemperatureBTService.m
@@ -0,0 +1,29 @@
+//
+//  DTTemperatureBTService.m
+//  Deanna
+//
+//  Created by Charles Choi on 12/18/12.
+//  Copyright (c) 2012 Yummy Melon Software. All rights reserved.
+//
+
+#import "DTTemperatureBTService.h"
+#import "DTCharacteristic.h"
+
+@implementation DTTemperatureBTService
+
+- (id)initWithName:(NSString *)oName {
+    self = [super init];
+    
+    if (self) {
+        self.name = oName;
+        
+        [self addCharacteristic:@"service" withOffset:kSensorTag_TEMPERATURE_SERVICE];
+        [self addCharacteristic:@"data" withOffset:kSensorTag_TEMPERATURE_DATA];
+        [self addCharacteristic:@"config" withOffset:kSensorTag_TEMPERATURE_CONFIG];
+    }
+    return self;
+}
+
+
+
+@end
