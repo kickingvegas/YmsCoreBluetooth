bea8bb53148f6010f11160e079da7872a3604740
Charles Choi <charles.choi@yummymelon.com>
2012-12-18 16:40:58 -0800

    First real implementation.

--- Deanna/DTSensorBTService.h
+++ Deanna/DTSensorBTService.h
@@ -6,8 +6,29 @@
 //  Copyright (c) 2012 Yummy Melon Software. All rights reserved.
 //
 
+
 #import <Foundation/Foundation.h>
+#import <CoreBluetooth/CoreBluetooth.h>
+#import "YMSCBUtils.h"
+
+@class DTCharacteristic;
 
 @interface DTSensorBTService : NSObject
+
+@property (nonatomic, strong) NSString *name;
+@property (nonatomic, strong) CBService *service;
+@property (nonatomic, assign) yms_u128_t base;
+
+// Contains DTCharacteristic values
+@property (nonatomic, strong) NSMutableDictionary *characteristicMap;
+
+
+- (void)addCharacteristic:(NSString *)cname withOffset:(int)addrOffset;
+
+- (NSArray *)characteristics;
+
+- (void)syncCharacteristics:(NSArray *)foundCharacteristics;
+
+- (DTCharacteristic *)findCharacteristic:(CBCharacteristic *)ct;
 
 @end
--- Deanna/DTSensorBTService.m
+++ Deanna/DTSensorBTService.m
@@ -7,7 +7,76 @@
 //
 
 #import "DTSensorBTService.h"
+#import "YMSCBUtils.h"
+#import "DTCharacteristic.h"
 
 @implementation DTSensorBTService
+
+
+- (id)init {
+    self = [super init];
+    
+    if (self) {
+        _characteristicMap = [[NSMutableDictionary alloc] init];
+        _base.hi = kSensorTag_BASE_ADDRESS_HI;
+        _base.lo = kSensorTag_BASE_ADDRESS_LO;
+        
+        
+    }
+    
+    return self;
+}
+
+- (void)addCharacteristic:(NSString *)cname withOffset:(int)addrOffset {
+    DTCharacteristic *dtc;
+    
+    yms_u128_t pbase = self.base;
+    
+    CBUUID *uuid = [YMSCBUtils createCBUUID:&pbase withIntOffset:addrOffset];
+    
+    dtc = [[DTCharacteristic alloc] initWithName:cname
+                                            uuid:uuid
+                                          offset:addrOffset];
+    
+    [self.characteristicMap setObject:dtc forKey:cname];
+}
+
+
+
+- (NSArray *)characteristics {
+    
+    NSArray *result = @[
+    [(DTCharacteristic *)(self.characteristicMap[@"data"]) uuid],
+    [(DTCharacteristic *)(self.characteristicMap[@"config"]) uuid],
+    ];
+
+    return result;
+}
+
+
+- (void)syncCharacteristics:(NSArray *)foundCharacteristics {
+    for (NSString *key in self.characteristicMap) {
+        DTCharacteristic *dtc = self.characteristicMap[key];
+        for (CBCharacteristic *ct in foundCharacteristics) {
+            if ([dtc.uuid isEqual:ct.UUID]) {
+                dtc.characteristic = ct;
+                break;
+            }
+        }
+    }
+}
+
+- (DTCharacteristic *)findCharacteristic:(CBCharacteristic *)ct {
+    DTCharacteristic *result;
+    for (NSString *key in self.characteristicMap) {
+        DTCharacteristic *dtc = self.characteristicMap[key];
+            
+        if ([dtc.characteristic.UUID isEqual:ct.UUID]) {
+            result = dtc;
+            break;
+        }
+    }
+    return result;
+}
 
 @end
