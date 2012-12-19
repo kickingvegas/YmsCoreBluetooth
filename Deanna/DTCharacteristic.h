8ff13f697bb5e9a25e38599fd7d6f1e0c58d0063
Charles Choi <charles.choi@yummymelon.com>
2012-12-18 16:39:46 -0800

    Initial Entry.

---
+++ Deanna/DTCharacteristic.h
@@ -0,0 +1,23 @@
+//
+//  DTCharacteristic.h
+//  Deanna
+//
+//  Created by Charles Choi on 12/18/12.
+//  Copyright (c) 2012 Yummy Melon Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreBluetooth/CoreBluetooth.h>
+
+@interface DTCharacteristic : NSObject
+
+@property (nonatomic, strong) NSString *name;
+@property (nonatomic, strong) CBUUID *uuid;
+@property (nonatomic, strong) CBCharacteristic *characteristic;
+@property (nonatomic, strong) NSNumber *offset;
+
+
+- (id)initWithName:(NSString *)oName uuid:(CBUUID *)oUUID offset:(int)addrOffset;
+
+
+@end
