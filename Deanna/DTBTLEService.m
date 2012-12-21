09fc58e6501f8d8922b47ffd338fef3b581a8e76
Charles Choi <charles.choi@yummymelon.com>
2012-12-20 20:33:27 -0800

    * Added initial hook to persist peripherals. * Code refactoring.

--- Deanna/DTBTLEService.m
+++ Deanna/DTBTLEService.m
@@ -11,6 +11,8 @@
 
 static DTBTLEService *sharedBTLEService;
 
+NSString * const DTBTLEServicePowerOffNotification = @"com.yummymelon.btleservice.power.off";
+
 @implementation DTBTLEService
 
 + (DTBTLEService *)sharedService {
@@ -25,9 +27,19 @@ static DTBTLEService *sharedBTLEService;
     self = [super init];
     
     if (self) {
+        _peripherals = [[NSMutableArray alloc] init];
         _manager = [[CBCentralManager alloc] initWithDelegate:self queue:nil];
-        [_manager scanForPeripheralsWithServices:nil options:nil];
-
+        
+        //[_manager scanForPeripheralsWithServices:nil options:nil];
+        
+//        yms_u128_t base;
+//        
+//        base.hi = kSensorTag_BASE_ADDRESS_HI;
+//        base.lo = kSensorTag_BASE_ADDRESS_LO;
+//
+//        NSArray *services = @[[YMSCBUtils createCBUUID:&base withIntOffset:kSensorTag_GAP_SERVICE_UUID]];
+//        
+//        [_manager scanForPeripheralsWithServices:services options:nil];
     }
     
     return self;
@@ -45,28 +57,95 @@ static DTBTLEService *sharedBTLEService;
 }
 
 
+- (void)persistPeripherals {
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    NSMutableArray *devices = [[NSMutableArray alloc] init];
+    
+    for (CBPeripheral *p in self.peripherals) {
+        CFStringRef uuidString = NULL;
+        
+        uuidString = CFUUIDCreateString(NULL, p.UUID);
+        if (uuidString) {
+            [devices addObject:(NSString *)CFBridgingRelease(uuidString)];
+        }
+    }
+
+    [userDefaults setObject:devices forKey:@"storedPeripherals"];
+    [userDefaults synchronize];
+}
+
+
+- (void)loadPeripherals {
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    NSArray *devices = [userDefaults arrayForKey:@"storedPeripherals"];
+    NSMutableArray *peripheralUUIDList = [[NSMutableArray alloc] init];
+    
+    if (![devices isKindOfClass:[NSArray class]]) {
+        // TODO - need right error handler
+        NSLog(@"No stored array to load");
+    }
+    
+    for (id uuidString in devices) {
+        if (![uuidString isKindOfClass:[NSString class]]) {
+            continue;
+        }
+        
+        CFUUIDRef uuid = CFUUIDCreateFromString(NULL, (CFStringRef)uuidString);
+        
+        if (!uuid)
+            continue;
+        
+        [peripheralUUIDList addObject:(id)CFBridgingRelease(uuid)];
+    }
+    
+    if ([peripheralUUIDList count] > 0) {
+        [self.manager retrievePeripherals:peripheralUUIDList];
+    }
+    else {
+        [self.manager scanForPeripheralsWithServices:nil options:nil];
+    }
+}
+
+
 
 #pragma mark CBCentralManagerDelegate Protocol Methods
 
 - (void)centralManagerDidUpdateState:(CBCentralManager *)central {
+    static CBCentralManagerState oldManagerState = -1;
+    
+    
     switch (central.state) {
         case CBCentralManagerStatePoweredOn:
+            NSLog(@"cbcm powered on");
+            //[self.manager scanForPeripheralsWithServices:nil options:nil];
+            [self loadPeripherals];
             
             break;
             
         case CBCentralManagerStateUnknown:
+            NSLog(@"cbcm unknown");
             break;
             
         case CBCentralManagerStatePoweredOff:
+            NSLog(@"cbcm powered off");
+
+            if (oldManagerState != -1) {
+                [[NSNotificationCenter defaultCenter] postNotificationName:DTBTLEServicePowerOffNotification
+                                                                    object:self];
+                
+            }
             break;
             
         case CBCentralManagerStateResetting:
+            NSLog(@"cbcm resetting");
             break;
             
         case CBCentralManagerStateUnauthorized:
+            NSLog(@"cbcm unauthorized");
             break;
             
         case CBCentralManagerStateUnsupported: {
+            NSLog(@"cbcm unauthorized");
             UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@""
                                                             message:@""
                                                            delegate:nil
@@ -77,31 +156,42 @@ static DTBTLEService *sharedBTLEService;
             break;
         }
     }
+    
+    oldManagerState = central.state;
 }
 
 
-- (void)centralManager:(CBCentralManager *)central didDiscoverPeripheral:(CBPeripheral *)peripheral advertisementData:(NSDictionary *)advertisementData RSSI:(NSNumber *)RSSI {
-    
-    // 3
-    //
-    
-    // 4
-    
-    if ([self isSensorTagPeripheral:peripheral]) {
-        NSLog(@"hey this matches;");
-        self.sensorTag = [[DTSensorTag alloc] init];
-        
-        //[self.sensorTag addPeripheralsObject:peripheral];
-        [self.sensorTag.peripherals addObject:peripheral];
-        
-        peripheral.delegate = self.sensorTag;
-        [central connectPeripheral:peripheral options:nil];
-        
-        [self.manager stopScan];
-        
-        self.sensorTagEnabled = YES;
+- (void)centralManager:(CBCentralManager *)central
+ didDiscoverPeripheral:(CBPeripheral *)peripheral
+     advertisementData:(NSDictionary *)advertisementData
+                  RSSI:(NSNumber *)RSSI {
+    if (![self.peripherals containsObject:peripheral]) {
+        if ([self isSensorTagPeripheral:peripheral]) {
+            if (!peripheral.isConnected) {
+                if (self.sensorTag == nil)
+                    self.sensorTag = [[DTSensorTag alloc] init];
+                [self.peripherals addObject:peripheral];
+                peripheral.delegate = self.sensorTag;
+                [central connectPeripheral:peripheral options:nil];
+                
+                [self.manager stopScan];
+                self.sensorTagEnabled = YES;
+            }
+        }
     }
-
+    
+//    if (![self.peripherals containsObject:peripheral]) {
+//        if ([self isSensorTagPeripheral:peripheral]) {
+//            [self.peripherals addObject:peripheral];
+//            self.sensorTag = [[DTSensorTag alloc] init];
+//            peripheral.delegate = self.sensorTag;
+//            
+//            [central connectPeripheral:peripheral options:nil];
+//
+//            [self.manager stopScan];
+//            self.sensorTagEnabled = YES;
+//        }
+//    }
     
 }
 
@@ -139,6 +229,37 @@ static DTBTLEService *sharedBTLEService;
 
 - (void)centralManager:(CBCentralManager *)central didRetrievePeripherals:(NSArray *)peripherals {
     NSLog(@"centralManager didRetrievePeripherals");
+    
+    
+    
+    for (CBPeripheral *peripheral in peripherals) {
+        if (![self.peripherals containsObject:peripheral]) {
+            if ([self isSensorTagPeripheral:peripheral]) {
+                if (!peripheral.isConnected) {
+                    if (self.sensorTag == nil)
+                        self.sensorTag = [[DTSensorTag alloc] init];
+                    [self.peripherals addObject:peripheral];
+                    peripheral.delegate = self.sensorTag;
+                    [central connectPeripheral:peripheral options:nil];
+                    
+                    [self.manager stopScan];
+                    self.sensorTagEnabled = YES;
+                }
+            }
+        }
+    }
+
+//        if ([self isSensorTagPeripheral:peripheral]) {
+//            [self.peripherals addObject:peripheral];
+//            if (self.sensorTag == nil)
+//                self.sensorTag = [[DTSensorTag alloc] init];
+//
+//            peripheral.delegate = self.sensorTag;
+//            [central connectPeripheral:peripheral options:nil];
+//            self.sensorTagEnabled = YES;
+//        }
+//    }
+//    
 }
 
 
