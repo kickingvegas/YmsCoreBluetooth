898ceed9a17771e350dc55877ddeb8c4a67802be
Charles Choi <charles.choi@yummymelon.com>
2013-01-19 12:19:20 -0800

    Rename to use YMSBluetoothService.

--- Deanna/DEAAppDelegate.m
+++ Deanna/DEAAppDelegate.m
@@ -8,7 +8,7 @@
 
 #import "DEAAppDelegate.h"
 #import "DEAHomeViewController.h"
-#import "DEABluetoothService.h"
+#import "YMSBluetoothService.h"
 
 @implementation DEAAppDelegate
 
@@ -40,7 +40,7 @@
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     
-    DEABluetoothService *btleService = [DEABluetoothService sharedService];
+    YMSBluetoothService *btleService = [YMSBluetoothService sharedService];
     [btleService persistPeripherals];
     
 }
@@ -66,7 +66,7 @@
 }
 
 - (void)initializeAppServices {
-    [DEABluetoothService sharedService];
+    [YMSBluetoothService sharedService];
 }
 
 @end
