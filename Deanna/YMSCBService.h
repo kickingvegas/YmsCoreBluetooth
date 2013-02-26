a4a481458c8fd300820a8e0317960f0c927cba70
Charles Choi <charles.choi@yummymelon.com>
2013-02-25 23:50:49 -0800

    Started adding AppleDoc comments.

--- Deanna/YMSCBService.h
+++ Deanna/YMSCBService.h
@@ -1,5 +1,5 @@
 //
-//  DTSensorBTService.h
+//  YMSCBService.h
 //  Deanna
 //
 //  Created by Charles Choi on 12/18/12.
@@ -13,24 +13,64 @@
 
 @class YMSCBCharacteristic;
 
+
+/**
+ Base class for TI SensorTag CoreBluetooth service definition.
+ */
+
 @interface YMSCBService : NSObject
 
+/// name of service
 @property (nonatomic, strong) NSString *name;
+
+/// pointer to CBService
 @property (nonatomic, strong) CBService *cbService;
+
+/// 128 bit base address struct
 @property (nonatomic, assign) yms_u128_t base;
+
+/** CoreBluetooth service is on */
+@property (nonatomic, assign) BOOL isOn;
+
+/** CoreBluetooth characteristics are synchronized */
 @property (nonatomic, assign) BOOL isEnabled;
 
-// Contains DEACharacteristic values
+///
+
+/// Holds (key, value pairs of (NSString, YMSCBCharacteristic) instances
 @property (nonatomic, strong) NSMutableDictionary *characteristicMap;
 
+/**
+ Initialize class instance.
+ @param oName name of service
+ @return YMSCBCharacteristic
+ */
 - (id)initWithName:(NSString *)oName;
 
+/**
+ Add YMSCBCharacteristic instance given address offset.
+ @param cname characteristic name
+ @param addrOffset offset value
+ */
 - (void)addCharacteristic:(NSString *)cname withOffset:(int)addrOffset;
 
+/**
+ Add YMSCBCharacteristic instance given address offset.
+ @param cname characteristic name
+ @param addr offset value
+ */
 - (void)addCharacteristic:(NSString *)cname withAddress:(int)addr;
 
+/**
+ Return array of YMSCBCharacteristic instances.
+ */
 - (NSArray *)characteristics;
 
+/**
+ Synchronize found CBCharacteristics with corresponding YMSCBCharacteristic containers.
+ 
+ @param foundCharacteristics array of CBCharacteristics
+ */
 - (void)syncCharacteristics:(NSArray *)foundCharacteristics;
 
 - (YMSCBCharacteristic *)findCharacteristic:(CBCharacteristic *)ct;
