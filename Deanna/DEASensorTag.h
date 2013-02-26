83af3bf2fc737b041ffe6593edb092977b374ac0
Charles Choi <charles.choi@yummymelon.com>
2013-02-26 09:31:12 -0800

    Initial Entry.

---
+++ Deanna/DEASensorTag.h
@@ -0,0 +1,13 @@
+//
+//  DEASensorTag.h
+//  Deanna
+//
+//  Created by Charles Choi on 2/26/13.
+//  Copyright (c) 2013 Yummy Melon Software. All rights reserved.
+//
+
+#import "YMSCBPeripheral.h"
+
+@interface DEASensorTag : YMSCBPeripheral
+
+@end
---
+++ Deanna/DEASensorTag.m
@@ -0,0 +1,13 @@
+//
+//  DEASensorTag.m
+//  Deanna
+//
+//  Created by Charles Choi on 2/26/13.
+//  Copyright (c) 2013 Yummy Melon Software. All rights reserved.
+//
+
+#import "DEASensorTag.h"
+
+@implementation DEASensorTag
+
+@end
