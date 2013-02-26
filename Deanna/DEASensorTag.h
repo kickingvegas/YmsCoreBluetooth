b7eeabca8251c626e36b6dc94195d86ffcd4d7eb
Charles Choi <charles.choi@yummymelon.com>
2013-02-25 23:48:04 -0800

    * Added AppleDoc comments. * Added shouldPingRSSI property

--- Deanna/DEASensorTag.h
+++ Deanna/DEASensorTag.h
@@ -12,13 +12,29 @@
 
 @class YMSCBService;
 
+
+/**
+ TI SensorTag object.
+ */
 @interface DEASensorTag : NSObject <CBPeripheralDelegate>
 
+/// 128 bit address base
 @property (nonatomic, assign) yms_u128_t base;
+
+/// Hold (key, value) pairs of (NSString, YMSCBService) instances
 @property (nonatomic, strong) NSDictionary *sensorServices;
+
+/// Pointer to CBPeripheral instance of a sensor tag.
 @property (nonatomic, strong) CBPeripheral *cbPeriperheral;
 
+@property (nonatomic, assign) BOOL shouldPingRSSI;
+
 - (id)initWithPeripheral:(CBPeripheral *)peripheral;
+
+/**
+ Generate array of CBUUID for all SensorTag CoreBluetooth services.
+ @return array of CBUUID services
+ */
 - (NSArray *)services;
 - (YMSCBService *)findService:(CBService *)service;
 - (void)updateRSSI;
