9959c2ae544496735e4a4c757530550a2666712a
Charles Choi <charles.choi@yummymelon.com>
2013-02-27 18:01:48 -0800

    Added Apache license

--- Deanna/DEATemperatureViewCell.m
+++ Deanna/DEATemperatureViewCell.m
@@ -1,10 +1,21 @@
 //
-//  DEATemperatureViewCell.m
-//  Deanna
+// Copyright 2013 Yummy Melon Software LLC
 //
-//  Created by Charles Choi on 2/27/13.
-//  Copyright (c) 2013 Yummy Melon Software. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//  Author: Charles Y. Choi <charles.choi@yummymelon.com>
+//
+
 
 #import "DEATemperatureViewCell.h"
 #import "DEASensorTag.h"
@@ -12,21 +23,17 @@
 
 @implementation DEATemperatureViewCell
 
-- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
-{
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if (self) {
         // Initialization code
-        self.ambientTemperatureLabel.text = @"--";
-        self.objectTemperatureLabel.text = @"--";
     }
     return self;
 }
 
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated
-{
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
-
+    
     // Configure the view for the selected state
 }
 
