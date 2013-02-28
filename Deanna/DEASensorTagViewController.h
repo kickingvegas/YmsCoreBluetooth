d1871ca5d9b9c6835bc010ca45a52a59d0bcc9a4
Charles Choi <charles.choi@yummymelon.com>
2013-02-27 18:03:30 -0800

    * Added support for accelerometer * Added Apache license

--- Deanna/DEASensorTagViewController.h
+++ Deanna/DEASensorTagViewController.h
@@ -1,15 +1,27 @@
 //
-//  DEASensorTagViewController.h
-//  Deanna
+// Copyright 2013 Yummy Melon Software LLC
 //
-//  Created by Charles Choi on 2/27/13.
-//  Copyright (c) 2013 Yummy Melon Software. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//  Author: Charles Y. Choi <charles.choi@yummymelon.com>
+//
+
 
 #import "DEABaseViewController.h"
 #import "YMSCBAppService.h"
 @class DEASensorTag;
 @class DEATemperatureViewCell;
+@class DEAAccelerometerViewCell;
 
 @interface DEASensorTagViewController : DEABaseViewController <UITableViewDelegate, UITableViewDataSource, YMSCBAppServiceDelegate>
 
@@ -18,6 +30,6 @@
 
 @property (strong, nonatomic) IBOutlet UITableView *sensorTableView;
 @property (strong, nonatomic) IBOutlet DEATemperatureViewCell *temperatureViewCell;
-@property (strong, nonatomic) IBOutlet UITableViewCell *accelerometerViewCell;
+@property (strong, nonatomic) IBOutlet DEAAccelerometerViewCell *accelerometerViewCell;
 
 @end
--- Deanna/DEASensorTagViewController.m
+++ Deanna/DEASensorTagViewController.m
@@ -1,14 +1,26 @@
 //
-//  DEASensorTagViewController.m
-//  Deanna
+// Copyright 2013 Yummy Melon Software LLC
 //
-//  Created by Charles Choi on 2/27/13.
-//  Copyright (c) 2013 Yummy Melon Software. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//  Author: Charles Y. Choi <charles.choi@yummymelon.com>
+//
+
 
 #import "DEASensorTagViewController.h"
 #import "DEACBAppService.h"
 #import "DEATemperatureViewCell.h"
+#import "DEAAccelerometerViewCell.h"
 
 @interface DEASensorTagViewController ()
 
@@ -50,6 +62,7 @@
     cbAppService.delegate = self;
 
     [self.temperatureViewCell configureWithSensorTag:self.sensorTag];
+    [self.accelerometerViewCell configureWithSensorTag:self.sensorTag];
 
     
 //    DEATemperatureService *ts = self.sensorTag.serviceDict[@"temperature"];
@@ -84,6 +97,7 @@
 
 - (void)viewWillDisappear:(BOOL)animated {
     [self.temperatureViewCell deconfigure];
+    [self.accelerometerViewCell deconfigure];
 }
 
 
