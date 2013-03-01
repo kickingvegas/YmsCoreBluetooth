81377021e2b6a6d70feaca8c26a80cc866a30542
Charles Choi <charles.choi@yummymelon.com>
2013-02-28 19:33:06 -0800

    Added barometer support.

---
+++ Deanna/DEABarometerViewCell.h
@@ -0,0 +1,51 @@
+//
+// Copyright 2013 Yummy Melon Software LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//  Author: Charles Y. Choi <charles.choi@yummymelon.com>
+//
+
+
+#import <UIKit/UIKit.h>
+@class DEABarometerService;
+@class DEASensorTag;
+
+/**
+ View and control logic for the SensorTag barometer service.
+ */
+@interface DEABarometerViewCell : UITableViewCell
+
+
+/// Pointer to service
+@property (strong, nonatomic) DEABarometerService *service;
+
+/// Enable notification
+@property (strong, nonatomic) IBOutlet UISwitch *notifySwitch;
+
+/// Display ambient temperature
+@property (strong, nonatomic) IBOutlet UILabel *ambientTemperatureLabel;
+
+/// Display object temperature
+@property (strong, nonatomic) IBOutlet UILabel *pressureLabel;
+@property (strong, nonatomic) IBOutlet UIButton *calibrateButton;
+
+- (IBAction)notifySwitchAction:(id)sender;
+- (IBAction)calibrateButtonAction:(id)sender;
+
+- (void)configureWithSensorTag:(DEASensorTag *)sensorTag;
+
+- (void)deconfigure;
+
+
+@end
---
+++ Deanna/DEABarometerViewCell.m
@@ -0,0 +1,117 @@
+//
+// Copyright 2013 Yummy Melon Software LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//  Author: Charles Y. Choi <charles.choi@yummymelon.com>
+//
+
+
+#import "DEABarometerViewCell.h"
+#import "DEASensorTag.h"
+#import "DEABarometerService.h"
+
+
+@implementation DEABarometerViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+
+- (IBAction)notifySwitchAction:(id)sender {
+    NSLog(@"notifySwitch");
+    if (self.notifySwitch.isOn) {
+        [self.service turnOn];
+    } else {
+        [self.service turnOff];
+    }
+}
+
+- (IBAction)calibrateButtonAction:(id)sender {
+    
+    if (self.service.isCalibrating == NO) {
+        [self.service writeByte:0x2
+          forCharacteristicName:@"config"
+                           type:CBCharacteristicWriteWithResponse];
+        self.service.isCalibrating = YES;
+    }
+    
+}
+
+
+- (void)configureWithSensorTag:(DEASensorTag *)sensorTag {
+    self.service = sensorTag.serviceDict[@"barometer"];
+    
+    for (NSString *key in @[@"ambientTemp", @"pressure", @"isOn", @"isEnabled", @"isCalibrating"]) {
+        [self.service addObserver:self forKeyPath:key options:NSKeyValueObservingOptionNew context:NULL];
+    }
+    
+    [self.notifySwitch setOn:self.service.isOn animated:YES];
+    [self.notifySwitch setEnabled:self.service.isEnabled];
+}
+
+- (void)deconfigure {
+    for (NSString *key in @[@"ambientTemp", @"pressure", @"isOn", @"isEnabled", @"isCalibrating"]) {
+        [self.service removeObserver:self forKeyPath:key];
+    }
+}
+
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+    
+    if (object != self.service) {
+        return;
+    }
+    
+    DEABarometerService *bs = (DEABarometerService *)object;
+    
+    if ([keyPath isEqualToString:@"ambientTemp"]) {
+        double temperatureC = [bs.ambientTemp doubleValue];
+        float temperatureF = (float)temperatureC * 9.0/5.0 + 32.0;
+        temperatureF = roundf(100 * temperatureF)/100.0;
+        self.ambientTemperatureLabel.text = [NSString stringWithFormat:@"%0.2f ℉", temperatureF];
+        
+    } else if ([keyPath isEqualToString:@"pressure"]) {
+        double pressure = [bs.pressure doubleValue];
+        float pressureRound = roundf(100 * pressure)/100.0;
+        
+        self.pressureLabel.text = [NSString stringWithFormat:@"%0.2f hPa", pressureRound];
+        
+    } else if ([keyPath isEqualToString:@"isOn"]) {
+        [self.notifySwitch setOn:bs.isOn animated:YES];
+        
+    } else if ([keyPath isEqualToString:@"isEnabled"]) {
+        [self.notifySwitch setEnabled:bs.isEnabled];
+        
+    } else if ([keyPath isEqualToString:@"isCalibrating"]) {
+        [self.calibrateButton setEnabled:!bs.isCalibrating];
+    }
+    
+    
+}
+
+
+
+
+@end
