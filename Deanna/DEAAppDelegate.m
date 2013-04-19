ed2c355aeef02b52ddfbf9a785bd0f6d6fb2814e
Charles Choi <charles.choi@yummymelon.com>
2013-04-18 18:00:31 -0700

    Removed call to persistPeripherals upon exit. Not necessary anymore.

--- Deanna/DEAAppDelegate.m
+++ Deanna/DEAAppDelegate.m
@@ -51,9 +51,6 @@
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     
-    DEACBAppService *btleService = [DEACBAppService sharedService];
-    [btleService persistPeripherals];
-    
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
