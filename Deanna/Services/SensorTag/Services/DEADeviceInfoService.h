a472063c501123e09971089b6bc34c75c46aad9a
Charles Choi <charles.choi@yummymelon.com>
2013-05-01 14:05:41 -0700

    Added documentation.

--- Deanna/Services/SensorTag/Services/DEADeviceInfoService.h
+++ Deanna/Services/SensorTag/Services/DEADeviceInfoService.h
@@ -19,18 +19,33 @@
 
 #import "YMSCBService.h"
 
+/**
+ TI SensorTag CoreBluetooth service definition for device information.
+ */
 @interface DEADeviceInfoService : YMSCBService
 
+/** @name Properties */
+/// System ID
 @property (nonatomic, strong) NSString *system_id;
+/// Model Number
 @property (nonatomic, strong) NSString *model_number;
+/// Serial Number
 @property (nonatomic, strong) NSString *serial_number;
+/// Firmware Revision
 @property (nonatomic, strong) NSString *firmware_rev;
+/// Hardware Revision
 @property (nonatomic, strong) NSString *hardware_rev;
+/// Software Revision
 @property (nonatomic, strong) NSString *software_rev;
+/// Manufacturer Name
 @property (nonatomic, strong) NSString *manufacturer_name;
+/// IEEE 11073 Certification Data
 @property (nonatomic, strong) NSString *ieee11073_cert_data;
 
-
+/** @name Read Device Information */
+/**
+ Issue set of read requests to obtain device information which is store in the class properties.
+ */
 - (void)readDeviceInfo;
 
 @end
