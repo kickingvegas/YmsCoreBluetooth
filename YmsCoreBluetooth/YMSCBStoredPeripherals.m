dfd06837a4c529c77cf1817670f7298e0ec0c157
Charles Choi <charles.choi@yummymelon.com>
2013-05-11 20:41:52 -0700

    Issue #39 - Support for not persisting discovered peripherals.
    Initial Entry.

---
+++ YmsCoreBluetooth/YMSCBStoredPeripherals.m
@@ -0,0 +1,110 @@
+// 
+// Copyright 2013 Yummy Melon Software LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//  Author: Charles Y. Choi <charles.choi@yummymelon.com>
+//
+
+#import "YMSCBStoredPeripherals.h"
+
+@implementation YMSCBStoredPeripherals
+
++ (void)initializeStorage {
+    
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    NSArray *devices = [userDefaults arrayForKey:@"storedPeripherals"];
+    if (devices == nil) {
+        [userDefaults setObject:@[] forKey:@"storedPeripherals"];
+        [userDefaults synchronize];
+    }
+
+}
+
++ (NSArray *)genPeripheralUUIDs {
+    NSMutableArray *result= [NSMutableArray new];
+    
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    NSArray *devices = [userDefaults arrayForKey:@"storedPeripherals"];
+    
+    for (id uuidString in devices) {
+        if (![uuidString isKindOfClass:[NSString class]]) {
+            continue;
+        }
+        
+        CFUUIDRef uuid = CFUUIDCreateFromString(NULL, (CFStringRef)uuidString);
+        
+        if (!uuid)
+            continue;
+        
+        [result addObject:(id)CFBridgingRelease(uuid)];
+    }
+    
+    return result;
+}
+
++ (void)saveUUID:(CFUUIDRef)UUID {
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    
+    NSArray *existingDevices = [userDefaults objectForKey:@"storedPeripherals"];
+    NSMutableArray *devices;
+    NSString *uuidString = nil;
+    if (UUID != nil) {
+        uuidString = UUID2STRING(UUID);
+
+        if (existingDevices != nil) {
+            devices = [[NSMutableArray alloc] initWithArray:existingDevices];
+            
+            if (uuidString) {
+                BOOL test = YES;
+                
+                for (NSString *obj in existingDevices) {
+                    if ([obj isEqualToString:uuidString]) {
+                        test = NO;
+                        break;
+                    }
+                }
+                
+                if (test) {
+                    [devices addObject:uuidString];
+                }
+            }
+        }
+        else {
+            devices = [[NSMutableArray alloc] init];
+            [devices addObject:uuidString];
+            
+        }
+        
+        [userDefaults setObject:devices forKey:@"storedPeripherals"];
+        [userDefaults synchronize];
+    }
+    
+}
+
++ (void)deleteUUID:(CFUUIDRef)UUID {
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    NSArray *devices = [userDefaults arrayForKey:@"storedPeripherals"];
+    NSMutableArray *newDevices = [NSMutableArray arrayWithArray:devices];
+    
+    NSString *uuidString = UUID2STRING(UUID);
+    
+    [newDevices removeObject:uuidString];
+    
+    [userDefaults setObject:newDevices forKey:@"storedPeripherals"];
+    [userDefaults synchronize];
+
+    
+}
+
+@end
