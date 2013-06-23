9f4a19309dcc3025b9a64626e9a59b16d18d584f
Charles Choi <charles.choi@yummymelon.com>
2013-06-23 12:30:55 -0700

    #62 - Initial Entry.

---
+++ Deanna/UI/Themes/DEADefaultTheme.h
@@ -0,0 +1,14 @@
+//
+//  DEADefaultTheme.h
+//  Deanna
+//
+//  Created by Charles Choi on 6/23/13.
+//  Copyright (c) 2013 Yummy Melon Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "DEATheme.h"
+
+@interface DEADefaultTheme : NSObject<DEATheme>
+
+@end
