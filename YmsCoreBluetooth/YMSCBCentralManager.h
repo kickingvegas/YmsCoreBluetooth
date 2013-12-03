a81cc5f8df1e76fa33450acf272c3dcf0f95ea2b
Charles Choi <charles.choi@yummymelon.com>
2013-12-02 23:16:21 -0800

    Uprev to 1.07.

--- YmsCoreBluetooth/YMSCBCentralManager.h
+++ YmsCoreBluetooth/YMSCBCentralManager.h
@@ -26,8 +26,8 @@
 #import "YMSCBUtils.h"
 
 // iOS7
-#define kYMSCBVersionNumber 1060
-#define kYMSCBVersion "1.060"
+#define kYMSCBVersionNumber 1070
+#define kYMSCBVersion "1.070"
 extern NSString *const YMSCBVersion;
 
 @class YMSCBPeripheral;
